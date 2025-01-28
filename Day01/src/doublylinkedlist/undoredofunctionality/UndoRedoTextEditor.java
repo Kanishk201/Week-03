6219dff1a9d8d6e330ccae722d961cb35965ba13
Kanishk201 <kanisyadav88780@gmail.com>
2025-01-28 22:48:21 +0530

    I have completed undo redo functionality program

---
+++ Day01/src/doublylinkedlist/undoredofunctionality/UndoRedoTextEditor.java
@@ -0,0 +1,29 @@
+package doublylinkedlist.undoredofunctionality;
+
+public class UndoRedoTextEditor {
+    public static void main(String[] args) {
+        TextEditor editor = new TextEditor(10);
+
+        editor.addState("State 1: Hello");
+        editor.addState("State 2: Hello World");
+        editor.addState("State 3: Hello World!");
+
+        editor.displayCurrentState();
+
+        editor.undo();
+        editor.displayCurrentState();
+
+        editor.undo();
+        editor.displayCurrentState();
+
+        editor.redo();
+        editor.displayCurrentState();
+
+        editor.addState("State 4: Hello Again!");
+        editor.displayCurrentState();
+
+        editor.undo();
+        editor.displayCurrentState();
+    }
+}
+
