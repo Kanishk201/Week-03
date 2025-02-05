7af84e6dcc73519746676622880f4d95b5494ca9
Kanishk201 <kanisyadav88780@gmail.com>
2025-02-05 23:17:33 +0530

    [Kanishk] Add. Junit test of removing duplicates from a string

---
+++ Day04and05/src/test/java/stringbuilder/removeduplicates/RemoveDuplicatesTest.java
@@ -0,0 +1,14 @@
+package stringbuilder.removeduplicates;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemoveDuplicatesTest {
+
+    @Test
+
+    public void removeDuplicatesTest(){
+        Assert.assertEquals("khf", RemoveDuplicates.removeDuplicates("kkhhff"));
+    }
+
+}
