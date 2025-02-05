d9824b1c239d60ed8c5b7958f72e79fdd5978ff8
Kanishk201 <kanisyadav88780@gmail.com>
2025-02-05 22:54:28 +0530

    [Kanishk] Add. Solution of reading file line by line

---
+++ Day04and05/src/main/java/filereader/readfilelinebyline/ReadFile.java
@@ -0,0 +1,19 @@
+package filereader.readfilelinebyline;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void readFile(String fileName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
+    }
+
+}
---
+++ Day04and05/src/main/java/filereader/readfilelinebyline/ReadFileMain.java
@@ -0,0 +1,8 @@
+package filereader.readfilelinebyline;
+
+public class ReadFileMain {
+    public static void main(String[] args) {
+        String fileName = "file.txt";
+        ReadFile.readFile(fileName);
+    }
+}
